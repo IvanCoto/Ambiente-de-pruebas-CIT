57a83a8681b56ae6e3b8911b0ff385c13985ca92
Iván Coto Jimenez <84398566+IvanCoto@users.noreply.github.com>
2022-08-06 16:23:20 -0600

    Agrego el ambiente de pruebas

---
+++ CIT-TEST-DOS/src/main/java/test1/Main.java
@@ -0,0 +1,11 @@
+package test1;
+
+import test1.pruebas.menu;
+
+public class Main {
+    public static void main(String[] args) {
+        menu menu = new menu();
+        
+        menu.mostrarMenu();
+    }  
+}
